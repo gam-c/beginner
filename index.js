14105cdf57c1ab1117ad39e5067287d694fff531
gam-c <stavish@gmail.com>
2022-06-01 14:24:46 -0400

    new

---
+++ index.js
@@ -0,0 +1,40 @@
+/* function add(num1, num2) {
+    return num1 + num2
+}
+
+console.log(add(3, 2))
+console.log(add(-3, -6))
+console.log(add(7, 3)) */
+
+/* function hoursInSeconds (hour) {
+    return 3600 * hour
+}
+
+console.log(hoursInSeconds(2))
+console.log(hoursInSeconds(10))
+console.log(hoursInSeconds(24)) */
+
+/* function calxPerimeter (length, width) {
+    return (length + width) * 2
+}
+
+console.log(calxPerimeter(6, 7))
+console.log(calxPerimeter(20, 10))
+console.log(calxPerimeter(2, 9)) */
+
+
+/* function calcTriangleArea (base, height) {
+    return  0.5 * (base * height)
+}
+
+console.log(calcTriangleArea(3, 2))
+console.log(calcTriangleArea(10, 10))
+console.log(calcTriangleArea(20, 20)) */
+
+function append(word) {
+    return word + "Frontend"
+}
+
+console.log(append("Apple"))
+console.log(append("Banana"))
+
